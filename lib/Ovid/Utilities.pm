1ae88529a98c9a5de71f29d99961b6ac71e1718a
Ovid <curtis_ovid_poe@yahoo.com>
2014-01-23 19:01:42 +0100

    A messy function for Capture::Tiny.

--- lib/Ovid/Utilities.pm
+++ lib/Ovid/Utilities.pm
@@ -3,9 +3,15 @@ package Ovid::Utilities;
 use strict;
 use warnings;
 use Term::ANSIColor;
-use Ovid::Exporter qw(red green);
+use Ovid::Exporter qw(red green messy);
 
 sub red   { color('red') . shift . color('reset') }
 sub green { color('green') . shift . color('reset') }
+
+sub messy {
+    warn "This goes to STDERR";
+    print "ok ... this goes to STDOUT\n";
+    return 42;
+}
 
 1;
