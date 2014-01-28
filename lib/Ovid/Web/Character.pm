db6525cf271d334c373a6db4371db66af08cf701
Ovid <curtis_ovid_poe@yahoo.com>
2014-01-28 11:41:21 +0100

    Refactor the character generation psgi application.

--- lib/Ovid/Web/Character.pm
+++ lib/Ovid/Web/Character.pm
@@ -2,199 +2,141 @@ package Ovid::Web::Character;
 
 use strict;
 use warnings;
+
 use Plack::Builder;
 use Plack::Request;
 use Template::Tiny;
-use List::Util 'sum';
+use File::Slurp 'read_file';
 use HTML::Entities 'encode_entities';
-use Ovid::Exporter qw( psgi_app );
+use Ovid::Exporter 'psgi_app';
 
 sub psgi_app {
-    return builder {
+
+    # this is our controller. Note that the "model" containing the data
+    # (*cough*) is moved into a generate_character() subroutine. If multiple
+    # parts of your application needed that, you'd put that into a separate
+    # module to make it easier to share, but we're keeping this simple.
+
+    # Also note that the templates have been moved into a templates/
+    # directory. That keeps them clean and allows other portions of the
+    # application to reuse them, too.
+
+    builder {
         mount '/' => sub {
             my $env     = shift;
             my $request = Plack::Request->new($env);
-            if ( '/character' eq $request->path ) {
-                my $template = character_template();
-                my $content;
-                Template::Tiny->new->process( \$template, {}, \$content );
-                my $response = $request->new_response(200);
-                $response->content_type('text/html');
-                $response->content($content);
-                return $response->finalize;
-            }
-            elsif ( '/' ne $request->path ) {
+            unless ( '/' eq $request->path ) {
                 return $request->new_response(404)->finalize;
             }
-            else {
-                my $template = character_display();
-                my $content;
-                if ( $request->param ) {
-                    my %adjustments_for = (
-                        profession => {
-                            programmer => {
-                                strength     => -3,
-                                intelligence => 8,
-                                health       => -2,
-                            },
-                            pilot    => { intelligence => 3 },
-                            redshirt => { strength     => 5 }
-                        },
-                        birthplace => {
-                            earth => {
-                                strength     => 2,
-                                intelligence => 0,
-                                health       => -2,
-                            },
-                            mars => { strength     => -5, health => 2 },
-                            vat  => { intelligence => 2,  health => -2 }
-                        },
-                    );
-                    my @errors;
-                    my %label_for = (
-                        profession => {
-                            pilot      => "Starship Pilot",
-                            programmer => "Programmer",
-                            redshirt   => "Doomed",
-                        },
-                        birthplace => {
-                            earth => "Earth",
-                            mars  => "Mars",
-                            vat   => "Vat 3-5LX",
-                        },
-                    );
-                    my %character = map {
-                        $_ => sum( map { 1 + int( rand(10) ) } 1 .. 3 )
-                    } qw/strength intelligence health/;
-
-                    foreach my $attribute (qw/name profession birthplace/) {
-                        if ( my $value = $request->param($attribute) ) {
-                            if ( my $adj
-                                = $adjustments_for{$attribute}{$value} )
-                            {
-                                while ( my ( $stat, $adjustment )
-                                    = each %$adj )
-                                {
-                                    $character{$stat} += $adjustment;
-                                }
-                            }
-                            $character{$attribute}
-                              = encode_entities(
-                                $label_for{$attribute}{$value} || $value );
-                        }
-                        else {
-                            push @errors => "\U$attribute is required";
-                        }
+            my $template = 'templates/character_display.tt';
+            my $content;
+            if ( $request->param ) {
+                my ( $character, $errors ) = generate_character($request);
+                $template = 'templates/character.tt' if @$errors;
+                $content = get_content(
+                    $template,
+                    {   character => $character,
+                        errors    => $errors,
                     }
-                    if ( 'redshirt' eq $request->param('profession') ) {
-                        $character{health} = 1;
-                    }
-                    $template = character_template() if @errors;
-                    Template::Tiny->new->process(
-                        \$template,
-                        { character => \%character, errors => \@errors },
-                        \$content
-                    );
-                }
-                else {
-                    Template::Tiny->new->process(
-                        \$template,
-                        { no_character_found => 1 },
-                        \$content
-                    );
-                }
-                my $response = $request->new_response(200);
-                $response->content_type('text/html');
-                $response->content($content);
-                return $response->finalize;
+                );
             }
+            else {
+                $content = get_content( $template, {} );
+            }
+            return response( $request, $content );
+        };
+        mount '/character' => sub {
+            my $env     = shift;
+            my $request = Plack::Request->new($env);
+            my $content = get_content('templates/character.tt');
+            return response( $request, $content );
         };
     };
 }
 
-sub character_template {
-    return <<'END';
-<html>
-  <head><title>Character Generation</title></head>
-  <body>
-    <fieldset>
-      <legend>Create your character</legend>
-[% FOREACH error IN errors %]
-      <p style="color:red; font-weight:bold">[% error %]</p>
-[% END %]
-      <form action="/" method="POST" name="awesome">
-        <table>
-          <tr><td>Name</td>
-            <td>
-              <input type="text" name="name" 
-                value="[% character.name %]" />
-            </td>
-          </tr>
-          <tr>
-            <td>Profession</td>
-            <td>
-              <select name="profession">
-                <option value="programmer">Programmer</option>
-                <option value="pilot">Starship Pilot</option>
-                <option value="redshirt">Security Officer</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>Birth place</td>
-            <td>Earth     <input type="radio" name="birthplace" value="earth" /> |
-                Mars      <input type="radio" name="birthplace" value="mars" /> |
-                Vat 3-5LX <input type="radio" name="birthplace" value="vat" />
-            </td>
-          </tr>
-        </table>
-        <div align="center">
-          <input type="submit" value="Submit" />
-        </div>
-      </form>
-    </fieldset>
-  </body>
-</html>
-END
+sub generate_character {
+    my $request         = shift;
+    my %adjustments_for = (
+        profession => {
+            programmer => {
+                strength     => -3,
+                intelligence => 8,
+                health       => -2,
+            },
+            pilot    => { intelligence => 3 },
+            redshirt => { strength     => 5 }
+        },
+        birthplace => {
+            earth => {
+                strength     => 2,
+                intelligence => 0,
+                health       => -2,
+            },
+            mars => { strength     => -5, health => 2 },
+            vat  => { intelligence => 2,  health => -2 }
+        },
+    );
+    my @errors;
+    my %label_for = (
+        profession => {
+            pilot      => "Starship Pilot",
+            programmer => "Programmer",
+            redshirt   => "Doomed",
+        },
+        birthplace => {
+            earth => "Earth",
+            mars  => "Mars",
+            vat   => "Vat 3-5LX",
+        },
+    );
+
+    my %value_for
+      = map { $_ => roll_dice() } qw/strength intelligence health/;
+
+    foreach my $attribute (qw/name profession birthplace/) {
+        if ( my $value = $request->param($attribute) ) {
+            if ( my $adj = $adjustments_for{$attribute}{$value} ) {
+                while ( my ( $stat, $adjustment ) = each %$adj ) {
+                    $value_for{$stat} += $adjustment;
+                }
+            }
+            $value_for{$attribute}
+              = encode_entities( $label_for{$attribute}{$value} || $value );
+        }
+        else {
+            push @errors => "\U$attribute is required";
+        }
+    }
+    if ( 'redshirt' eq $request->param('profession') ) {
+        $value_for{health} = 1;
+    }
+    return \%value_for, \@errors;
 }
 
-sub character_display {
-    return <<'END';
-<html>
-  <head><title>The Awesome "This does nothing!" Game</title></head>
-  <body>
-    <fieldset>
-[% IF no_character_found %]
-      <legend>Create your character</legend>
-      <p>
-        <a href="/character">Click here to create a character</a>
-      </p>
-[% ELSE %]
-      <legend>Character Stats</legend>
-      <table style="border-spacing:5px;">
-        <tr><td>Name</td>        
-            <td>[% character.name %]</td></tr>
-        <tr><td>Profession</td>  
-            <td>[% character.profession %]</td></tr>
-        <tr><td>Birth place</td> 
-            <td>[% character.birthplace %]</td></tr>
-        <tr><td>Strength</td>    
-            <td>[% character.strength %]</td></tr>
-        <tr><td>Intelligence</td>
-            <td>[% character.intelligence %]</td></tr>
-        <tr><td>Health</td>      
-            <td>[% character.health %]</td></tr>
-      </table>
-      <p>
-        <a href="/character">
-          Click here to generate another character.
-        </a>
-      </p>
-[% END %]
-    </fieldset>
-  </body>
-</html>
-END
+sub roll_dice {
+    my $total = 0;
+    for ( 1 .. 3 ) {
+        $total += 1 + int( rand(10) );
+    }
+    return $total;
+}
+
+sub response {
+    my ( $request, $content ) = @_;
+    my $response = $request->new_response(200);
+    $response->content_type('text/html');
+    $response->content($content);
+    return $response->finalize;
+}
+
+sub get_content {
+    my ( $file, $vars ) = @_;
+    $vars ||= {};
+    my $template_code = read_file($file);
+    my $template      = Template::Tiny->new;
+    my $output;
+    $template->process( \$template_code, $vars, \$output );
+    return $output;
 }
 
 1;
