7e9da89ae89152ecb0fd8877d26e10535f6fa936
Ovid <curtis_ovid_poe@yahoo.com>
2014-02-06 00:00:00 +0100

    Turn get_html_comments into a modulino and start tests.

--- bin/get_html_comments.pl
+++ bin/get_html_comments.pl
@@ -1,13 +1,25 @@
 use 5.010;
+use strict;
+use warnings;
+
+package Modulino::HTMLComments;
 use WWW::Mechanize;
 use HTML::TokeParser::Simple;
+use Carp;
 
-my $url = shift @ARGV or die "Usage: $0 http://www.example.com/";
+__PACKAGE__->run(@ARGV) unless caller();
 
-my $mech = WWW::Mechanize->new;
-$mech->get($url);
+sub run {
+    my $class = shift;
+    my $url   = shift @_ or die "Usage: $0 http://www.example.com/";
 
-my $parser = HTML::TokeParser::Simple->new( string => $mech->content );
-while ( my $token = $parser->get_token ) {
-    say $token->as_is if $token->is_comment;
+    my $mech = WWW::Mechanize->new;
+    $mech->get($url);
+
+    my $parser = HTML::TokeParser::Simple->new( string => $mech->content );
+    while ( my $token = $parser->get_token ) {
+        say $token->as_is if $token->is_comment;
+    }
 }
+
+1;
