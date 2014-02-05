3ad795b7f7acd0bb05f6c68bcfd0b53675104a22
Ovid <curtis_ovid_poe@yahoo.com>
2014-02-06 00:00:00 +0100

    Test::Deep practice

--- lib/Ovid/DataStructures.pm
+++ lib/Ovid/DataStructures.pm
@@ -1,7 +1,27 @@
 use strict;
 use warnings;
+
 package Ovid::DataStructures;
-use Ovid::Exporter qw(to_json from_json); # re-export from JSON
+use Ovid::Exporter qw(to_json from_json get_customer);   # re-export from JSON
+use Ovid::Exam1 qw(roll_dice);
 use JSON qw(to_json from_json);
+
+sub get_customer {
+    my $name      = shift;
+    my %customers = (
+        bob     => [ 'Bob Dobbs',      [qw/religion/] ],
+        sally   => [ 'Sally Ride',     [qw/teaching/] ],
+        elon    => [ 'Elon Musk',      [qw/space technology business/] ],
+        ricardo => [ 'Ricardo Semler', [qw/business maverick/] ],
+    );
+    return {} unless my $customer = $customers{$name};
+    return {
+        customer_id => sprintf( "%06d" => int( rand(1_000_000) ) ),
+        name        => $customer->[0],
+        categories  => $customer->[1],
+        age    => 20 + roll_dice( { times => 3 } ),
+        active => int( rand(1) + .5 ),
+    };
+}
 
 1;
