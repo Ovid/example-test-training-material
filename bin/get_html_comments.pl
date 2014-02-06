3028f814c90670da72a4fe78e1f228b2990a2624
Ovid <curtis_ovid_poe@yahoo.com>
2014-02-06 15:14:02 +0100

    Show how we refactor a script using a modulino and tests.

--- bin/get_html_comments.pl
+++ bin/get_html_comments.pl
@@ -3,22 +3,53 @@ use strict;
 use warnings;
 
 package Modulino::HTMLComments;
-use WWW::Mechanize;
-use HTML::TokeParser::Simple;
 use Carp;
+use HTML::TokeParser::Simple;
+use Net::Ping;
+use WWW::Mechanize;
 
 __PACKAGE__->run(@ARGV) unless caller();
 
 sub run {
     my $class = shift;
-    my $url   = shift @_ or die "Usage: $0 http://www.example.com/";
+    print join "\n" => $class->get_comments( $class->get_html(shift) );
+}
 
+sub get_html {
+    my ( $class, $url ) = @_;
+    unless ($url) {
+        croak("get_html() requires a URL");
+    }
     my $mech = WWW::Mechanize->new;
     $mech->get($url);
+    return $mech->content;
+}
 
-    my $parser = HTML::TokeParser::Simple->new( string => $mech->content );
+sub get_comments {
+    my ( $class, $html ) = @_;
+    my $parser = HTML::TokeParser::Simple->new( string => $html );
+    my @comments;
     while ( my $token = $parser->get_token ) {
-        say $token->as_is if $token->is_comment;
+        push @comments => $token->as_is if $token->is_comment;
+    }
+    return @comments;
+}
+
+sub have_internet {
+    # this will hang a bit if we don't have internet
+    eval "use Net::Ping::External";
+    if ( my $error = $@ ) {
+        my $builder = Test::Builder->new;
+        $builder->note("Cannot load Net::Ping::External: $error.");
+        $builder->diag(
+            "Defaulting to the assumption that we have internet access");
+        return 1;
+    }
+    else {
+        my $net = Net::Ping->new("external");
+
+        # ping Google's DNS server
+        return $net->ping('8.8.8.8') ? 1 : 0;
     }
 }
 
